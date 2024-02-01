53d1c1d0fecaa3bf387ea4a6c04047cbdc2d7a31
Hongbusi <coderhbs@gmail.com>
2024-02-01 20:26:13 +0800

    feat: init shadcn ui

--- nest-oss-upload/client/src/App.tsx
+++ nest-oss-upload/client/src/App.tsx
@@ -1,7 +1,11 @@
+import { Input } from '@/components/ui/input'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="flex justify-center items-center h-screen">
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Input id="picture" type="file" />
+      </div>
+    </main>
   )
 }
